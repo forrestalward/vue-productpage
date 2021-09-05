5d0a5a913475b7e56a56ece611c18381e7869612
Forrest Alward <58065343+forrestalward@users.noreply.github.com>
2021-09-05 17:09:50 +0300

    cart buttons and hover effects

--- main.js
+++ main.js
@@ -1,26 +1,38 @@
-var product = "socks"; 
+//Create a new button and method to decrement the value of `cart`.
 
 var app = new Vue({
-    el: '#app', 
+    el: '#app',
     data: {
-        product: "Socks",
-        description: "These are some high quality socks",
-        image: "assets/green-socks.jpeg", 
-        altText: "A pair of Socks", 
-        link: "https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg", 
-        inventory: 100, 
-        onSale: true, 
-        details: ["80% cotton", "20% Polyester", "Gender-neutral"], 
-        variants: [{
-            variantId: 2234, 
-            variantColor: "green"
+      product: 'Socks',
+      image: 'assets/green-socks.jpeg',
+      inStock: true,
+      details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+      variants: [
+        {
+          variantId: 2234,
+          variantColor: 'green',
+          variantImage: 'assets/green-socks.jpeg'
         },
         {
-            variantId: 2235, 
-            variantColor: "blue"
-        } 
-        ],
-        sizes: ["XS", "S", "M", "L", "XL", "XXL"]
-        
+          variantId: 2235,
+          variantColor: 'blue',
+          variantImage: 'assets/socks-blue.jpeg'
+        }
+      ],
+      cart: 0
+    },
+    // methods here
+    methods: {
+      addToCart() {
+        this.cart += 1
+      },
+      updateProduct(variantImage) {
+        this.image = variantImage
+      },
+      removeFromCart() {
+        this.cart -= 1
+      }
     }
-})  
+  })
+  
+  
