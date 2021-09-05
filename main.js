2e4ad3bcdcbab739437070ff3439f9042b7eeb65
Forrest Alward <58065343+forrestalward@users.noreply.github.com>
2021-09-05 13:15:45 +0300

    added color variants

--- main.js
+++ main.js
@@ -7,6 +7,18 @@ var app = new Vue({
         description: "These are some high quality socks",
         image: "assets/green-socks.jpeg", 
         altText: "A pair of Socks", 
-        link: "https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg"
+        link: "https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg", 
+        inventory: 100, 
+        onSale: true, 
+        details: ["80% cotton", "20% Polyester", "Gender-neutral"], 
+        variants: [{
+            variantId: 2234, 
+            variantColor: "green"
+        },
+        {
+            variantId: 2235, 
+            variantColor: "blue"
+        } 
+    ]
     }
 })  
