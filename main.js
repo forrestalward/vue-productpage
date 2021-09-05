4f50b826f717492c25ac6f19fec62cc2c307c3f2
Forrest Alward <58065343+forrestalward@users.noreply.github.com>
2021-09-05 09:19:19 +0300

    adding more data to product

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ var app = new Vue({
     el: '#app', 
     data: {
         product: "Socks",
-        description: "These are some high quality socks"
+        description: "These are some high quality socks",
+        image: "assets/green-socks.jpeg", 
+        altText: "A pair of Socks", 
+        link: "https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg"
     }
 })  
